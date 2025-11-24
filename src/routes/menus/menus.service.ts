f5cfb09bd4f43c6fd09a29a6a3969fb45094bcf1
Annop Srichan <annopsrichan@gmail.com>
2025-11-24 12:57:25 +0700

    [Fix]: variable and path

--- src/routes/menus/menus.service.ts
+++ src/routes/menus/menus.service.ts
@@ -1,12 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { MenuRepository } from 'src/repository/menus/menus.repository';
 
 @Injectable()
 export class MenuService {
   constructor(
-    @Injectable(MenuRepository) private readonly menuRepository: MenuRepository,
-  ) 
-  { }
+    @Inject(MenuRepository) private readonly menuRepository: MenuRepository,
+  ) { }
 
   async getMenu(permissions: string[]) {
     const menus = await this.menuRepository.getMenusByPermissions(permissions);
