e5828b35f91624856a5f1cfc21da6cda9bea0a25
annops-entro <annop.s@entronica.in.th>
2026-07-03 14:45:31 +0700

    refactor: replace ValidationPipe with custom AppValidationPipe for enhanced validation

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,12 @@ import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
-import { ValidationPipe } from '@nestjs/common';
+import { AppValidationPipe } from './common/pipes/app-validation.pipe';
 
 async function bootstrap() {
   console.log('Environment Variables:',process.env.TEST_KEY);
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true
-    }),
-  );
+  app.useGlobalPipes(new AppValidationPipe());
   app.setGlobalPrefix('api');
   app.use(cookieParser());
   app.useGlobalFilters(new HttpExceptionFilter());
