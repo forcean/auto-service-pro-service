704f8458db1438e32435f68aac401d21a504f078
force an <annopsrichan@gmail.com>
2026-07-24 22:15:48 +0700

    feat(menus): enhance menu permissions handling and structure with new interfaces

--- src/routes/menus/menus.service.ts
+++ src/routes/menus/menus.service.ts
@@ -1,26 +1,68 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { MenuRepository } from 'src/repository/menus/menus.repository';
 import { AuthUser } from 'src/types/user.type';
+import { IMenuNode, IMenuResponse } from './interfaces/menus.interface';
+import { MenuDocument, MenuEntity } from 'src/repository/menus/menus.schema';
 
 @Injectable()
 export class MenuService {
   constructor(
     @Inject(MenuRepository) private readonly menuRepository: MenuRepository,
-  ) { }
+  ) {}
 
-  async getMenu(authUser: AuthUser) {
-    const menus = await this.menuRepository.getMenusByPermissions(authUser.permissions);
+  async getMenu(authUser: AuthUser): Promise<IMenuResponse> {
+    const menus = await this.menuRepository.getMenusByPermissions(
+      authUser.permissions,
+    );
+
     return {
       role: authUser.role,
-      menus: menus.map(menu => ({
+      menus: this.buildMenuTree(menus),
+    };
+  }
+
+  private buildMenuTree(menus: MenuDocument[]): IMenuNode[] {
+    const menuMap = new Map<string, IMenuNode>();
+    menus.forEach((menu) => {
+      menuMap.set(menu._id.toString(), {
         seq: menu.seq,
         key: menu.key,
         displayName: menu.displayName,
         icon: menu.icon,
         endpoint: menu.endpoint,
         activeFlag: menu.activeFlag,
-        children: menu.children
-      }))
-    };
+        children: [],
+      });
+    });
+    const roots: IMenuNode[] = [];
+    menus.forEach((menu) => {
+      const current = menuMap.get(menu._id.toString());
+
+      if (!current) {
+        return;
+      }
+
+      if (menu.parentId) {
+        const parent = menuMap.get(menu.parentId.toString());
+
+        if (parent) {
+          parent.children.push(current);
+        }
+      } else {
+        roots.push(current);
+      }
+    });
+    this.sortMenus(roots);
+
+    return roots;
+  }
+
+  private sortMenus(items: IMenuNode[]): void {
+    items.sort((a, b) => a.seq - b.seq);
+    items.forEach((item) => {
+      if (item.children.length > 0) {
+        this.sortMenus(item.children);
+      }
+    });
   }
 }
